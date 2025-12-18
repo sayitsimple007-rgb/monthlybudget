c02459806e790ac1849f481b4c1abf3c46feb7fd
rocks28 <rocks28@gmail.com>
2025-12-18 14:19:30 -0500

    add cors
    add cors

--- src/main/java/com/example/monthlybudget/MonthlybudgetApplication.java
+++ src/main/java/com/example/monthlybudget/MonthlybudgetApplication.java
@@ -2,6 +2,9 @@ package com.example.monthlybudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class    MonthlybudgetApplication {
@@ -10,4 +13,16 @@ public class    MonthlybudgetApplication {
         SpringApplication.run(MonthlybudgetApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")         // allow all endpoints
+                        .allowedOrigins("http://localhost:3000")  // your React app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
